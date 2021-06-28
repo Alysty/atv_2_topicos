f2684e06062fa63d588a8ce6ab02f6afd79cf16f
Alysty <pppjw@protonmail.ch>
2021-06-28 19:33:20 -0300

    Finished all tests for the first exercise and made the name of each test better for understading

--- __tests__/Tests_1.js
+++ __tests__/Tests_1.js
@@ -8,41 +8,15 @@ test("final negativo retorna -1", ()=>{
     expect(Aleatorio.gerarNumeroAleatorio(3,1)).toBe(-1)
 })
 
-test("inicio negativo retorna -1", ()=>{
+test("inicio maior que final retorna -1", ()=>{
     expect(Aleatorio.gerarNumeroAleatorio(3,1)).toBe(-1)
 
 })
+test("inicio igual ao final retorna -1", ()=>{
+    expect(Aleatorio.gerarNumeroAleatorio(3,3)).toBe(-1)
 
-
-/*
-*
-*import java.util.Random;
-public class Aleatorio {
-O metodo deve receber dois inteiros que representam o inicio e o fim de um
-intervalo e
- * retornar um numero aleatorio que se encontra dentro do intervalo estabelecido,
- * ou seja, [inicio, fim]. Caso o inicio do intervalo ou o fim do intervalo sejam
- * menor que zero, o metodo deve retornar -1. O metodo tambem retorna -1 quando
-o inteiro
- * representando o inicio do intervalo for maior ou igual ao inteiro representando o
- * fim do intervalo.
- *
- *
-public int gerarNumeroAleatorio(int inicio, int fim) {
-    if () {
-        return -1;
-    }
-
-    if (inicio >= fim) {
-        return -1;
-    }
-    int diff = fim - inicio + 1;
-    Random random = new Random();
-    int tInt = random.nextInt(diff); //esse método retorna um número aleatorio
-    //entre 0 e diff [ 0, diff [
-    return inicio + tInt;
-}
-}
-*
-*
-* */
+})
+test("inicio 1 e final 3 retorna numero entre 200 e 3000", ()=>{
+    expect(Aleatorio.gerarNumeroAleatorio(200,3000)).toBeGreaterThanOrEqual(200)
+    expect(Aleatorio.gerarNumeroAleatorio(200,3000)).toBeLessThanOrEqual(3000)
+})
