9322000965278ffd0dfd1bd6edd852dd78522d51
Alysty <pppjw@protonmail.ch>
2021-06-30 19:21:09 -0300

    fixed problem with ex 1

--- ex1.js
+++ ex1.js
@@ -7,7 +7,7 @@ const Aleatorio = {
         if(inicio >= fim){
             return -1;
         }
-        return Math.floor(Math.random() * fim - inicio + 1) + inicio
+        return Math.floor((Math.random() * inicio ) + 1)
     }
 }
 
